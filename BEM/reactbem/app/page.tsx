1462480f5e013476994cfb75a4b0815d0f4ca3b8
SMNahuel <nahuelsan96@gmail.com>
2024-02-09 21:16:55 -0300

    solved desing for frontmentor

--- BEM/reactbem/app/page.tsx
+++ BEM/reactbem/app/page.tsx
@@ -17,9 +17,7 @@ export default function Home() {
           <Image alt="imgProfile" className="profile__img" width={100} height={100}  src="/81668762.jpeg"/>
           <h2 className="profile__name">Nahuel Sanchez</h2>
           <h3 className="profile__ubication">Corrientes, Argentina</h3>
-          <p className="profile__occupation">
-            "Front End Developer and avid reader"
-          </p>
+          <p className="profile__occupation">"Front End Developer and avid reader"</p>
           {profile.map((data, key) => {
             return <ButtonProfile key={key} profile={data}/>;
           })}
